ef65fa43f96bcd72d74c18fd0b6ac69ea4af1ca9
Huynh Huu Loc <huynhhuuloc129@gmail.com>
2022-07-07 16:43:44 +0700

    Update test for transfer

--- db/query/entries.sql
+++ db/query/entries.sql
@@ -5,7 +5,6 @@ INSERT INTO entries(
     $1, $2
 ) RETURNING *;
 
-
 -- name: GetEntry :one
 SELECT * FROM entries
 WHERE id = $1 LIMIT 1;
--- db/query/transfers.sql
+++ db/query/transfers.sql
@@ -5,7 +5,6 @@ INSERT INTO transfers(
     $1, $2, $3
 ) RETURNING *;
 
-
 -- name: GetTransfer :one
 SELECT * FROM transfers
 WHERE id = $1 LIMIT 1;
